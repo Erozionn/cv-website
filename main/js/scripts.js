61cd0149b7f5bb862796da05669717fef1679030
erozionn <erozionnarts@gmail.com>
2021-12-16 10:38:08 -0500

    update

--- main/js/scripts.js
+++ main/js/scripts.js
@@ -1,10 +1,14 @@
 // document.addEventListener('ready', () => {
-  const animWrap = document.querySelector('.animation')
-  console.log(animWrap)
-  for (let i = 0; i < 30; i++) {
-    const div = document.createElement('div');
-    div.className = 'point'
-    animWrap.appendChild(div)
-  }
-  
-// })
+const animation = document.querySelector('.animation')
+
+for (let i = 0; i < 30; i++) {
+  const div = document.createElement('div');
+  div.className = 'point'
+  animation.appendChild(div)
+}
+
+document.querySelector(".wrap").addEventListener('mousemove', function(e) {
+  let _w = window.innerWidth;
+  let _mouseX = e.clientX;
+  animation.style.left = `${(_w - _mouseX) / 80}px`
+})
